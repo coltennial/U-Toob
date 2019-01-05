f0d8fb66c6bcb07961fa103e2381dee6b447287f
Harlan Evans <harlanevans5@gmail.com>
2019-01-05 12:05:36 -0700

    finished fetchuser protectedroute login and register

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,24 @@ import React, { Fragment, } from 'react';
 import { Switch, Route, } from 'react-router-dom';
 import { Container, } from 'semantic-ui-react';
 import Home from './components/Home';
+import Login from './components/Login';
+import FetchUser from './components/FetchUser';
+import ProtectedRoute from './components/ProtectedRoute';
 
 const App = () => (
 <Fragment>
   <Navbar />
+  <FetchUser>
+
   <Container>
     <Switch>
-      <Route exact path='/' component={Home} />
+      <ProtectedRoute exact path='/' component={Home} />
       <Route exact path='/' component={Home} />
       <Route exact path='/' component={Home} />
       <Route component={NoMatch} />
     </Switch>
   </Container>
+  </FetchUser>
 </Fragment>
 )
 
