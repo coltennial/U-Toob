77db89561dc93e24da1c3c8d0020bb8ddecfd6bd
Colton Davis <colt822lavay@outlook.com>
2019-01-05 15:01:54 -0700

    Add Video Model

--- app/controllers/api/videos_controller.rb
+++ app/controllers/api/videos_controller.rb
@@ -1,6 +1,35 @@
 class Api::VideosController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_video, only: [:show, :destroy]
 
   def index 
     render json: Video.all
   end 
+
+  def show 
+    render json: @video
+  end
+
+  def create 
+    video = Video.new(video_params)
+    if video.save 
+      render json: video 
+    else 
+      render json: video.errors
+    end
+  end
+
+  def destroy 
+    @video.destroy 
+  end
+
+  private 
+
+  def set_video 
+    @video = Video.find(params[:id])
+  end
+
+  def video_params 
+    params.require(params[:id])
+  end
 end
